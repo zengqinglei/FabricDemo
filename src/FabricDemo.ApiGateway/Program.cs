c006c6c31ac98a94dfeeabe32c55da333e12dd77
zengql <zengql@live.cn>
2018-08-07 00:09:39 +0800

    新增支持docker发布

--- src/FabricDemo.ApiGateway/Program.cs
+++ src/FabricDemo.ApiGateway/Program.cs
@@ -3,13 +3,16 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace FabricDemo.ApiGateway
 {
+    /// <inheritdoc />
     public class Program
     {
+        /// <inheritdoc />
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
         }
 
+        /// <inheritdoc />
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
--- src/FabricDemo.ProductService/Program.cs
+++ src/FabricDemo.ProductService/Program.cs
@@ -1,22 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace FabricDemo.ProductService
 {
+    /// <inheritdoc />
     public class Program
     {
+        /// <inheritdoc />
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
         }
 
+        /// <inheritdoc />
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
